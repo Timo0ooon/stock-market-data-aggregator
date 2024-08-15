e12be262b1bca79b93554b0d057abe4bd8900518
CNVMXM <maximkonovalov2613@gmail.com>
2024-08-15 17:59:09 +0300

    config-service patch.v3

--- api-gateway/src/main/java/com/cnvmxm/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/cnvmxm/apigateway/ApiGatewayApplication.java
@@ -2,7 +2,7 @@ package com.cnvmxm.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.*;
 @SpringBootApplication
 public class ApiGatewayApplication {
 
