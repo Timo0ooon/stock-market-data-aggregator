a10f39fbdf3f1e2b96bf3c46a3770cf6ac3d336c
CNVMXM <maximkonovalov2613@gmail.com>
2024-08-16 09:37:54 +0300

    Revert "config-service patch.v4"
    This reverts commit cee98b82

--- api-gateway/src/main/java/com/cnvmxm/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/cnvmxm/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,8 @@ package com.cnvmxm.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+import org.springframework.cloud.netflix.eureka.*;
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
